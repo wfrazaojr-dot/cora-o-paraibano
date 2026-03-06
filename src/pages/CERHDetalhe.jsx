7925802f67ff7bc79737e2170f8d34590fadb8fe
WALBER ALVES FRAZÃO JR <wfrazaojr@gmail.com>
2026-03-06 22:34:23 +0000

    File changes

--- src/pages/CERHDetalhe.jsx
+++ src/pages/CERHDetalhe.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
