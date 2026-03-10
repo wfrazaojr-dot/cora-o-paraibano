03dedbfb87f7a59af49e13f340d1f58c70fd1f17
WALBER ALVES FRAZÃO JR <wfrazaojr@gmail.com>
2026-03-10 00:24:28 +0000

    File changes

---
+++ src/components/data/cidadesParaiba.jsx
@@ -0,0 +1,56 @@
+export const CIDADES_POR_MACRO = {
+  "Macro 1": [
+    "Alagoinha", "Alhandra", "Araçagi", "Araruna", "Baía da Traição", "Bananeiras",
+    "Bayeux", "Belém", "Borborema", "Caaporã", "Cabedelo", "Cacimba de Dentro",
+    "Caiçara", "Caldas Brandão", "Capim", "Casserengue", "Conde",
+    "Cruz do Espírito Santo", "Cuité de Mamanguape", "Cuitegi", "Curral de Cima",
+    "Dona Inês", "Duas Estradas", "Guarabira", "Gurinhém", "Itabaiana",
+    "Itapororoca", "Jacaraú", "João Pessoa", "Juripiranga", "Lagoa de Dentro",
+    "Logradouro", "Lucena", "Mamanguape", "Marcação", "Mari", "Mataraca",
+    "Mogeiro", "Mulungu", "Natuba", "Pedras de Fogo", "Pedro Régis", "Pilar",
+    "Pilões", "Pilõezinhos", "Pirpirituba", "Pitimbu", "Riachão",
+    "Riachão do Poço", "Rio Tinto", "Salgado de São Félix", "Santa Rita",
+    "São José dos Ramos", "São Miguel de Taipu", "Sapé", "Serra da Raiz",
+    "Serraria", "Sertãozinho", "Sobrado", "Solânea", "Tacima"
+  ],
+  "Macro 2": [
+    "Alagoa Grande", "Alagoa Nova", "Alcantil", "Algodão de Jandaíra", "Amparo",
+    "Arara", "Areia", "Areial", "Aroeiras", "Assunção", "Barra de Santa Rosa",
+    "Barra de Santana", "Barra de São Miguel", "Boa Vista", "Boqueirão",
+    "Cabaceiras", "Camalaú", "Campina Grande", "Caraúbas", "Caturité", "Congo",
+    "Coxixola", "Cubati", "Cuité", "Desterro", "Esperança", "Fagundes",
+    "Frei Martinho", "Gado Bravo", "Gurjão", "Juarez Távora", "Juazeirinho",
+    "Junco do Seridó", "Lagoa Seca", "Livramento", "Massaranduba", "Matinhas",
+    "Montadas", "Monteiro", "Nova Floresta", "Nova Palmeira", "Olivedos",
+    "Ouro Velho", "Parari", "Pedra Lavrada", "Picuí", "Pocinhos", "Prata",
+    "Puxinanã", "Queimadas", "Remígio", "Riacho de Santo Antônio",
+    "Santa Cecília", "Santo André", "São Bentinho", "São Domingos do Cariri",
+    "São João do Cariri", "São João do Tigre", "São José dos Cordeiros",
+    "São Sebastião de Lagoa de Roça", "São Sebastião do Umbuzeiro",
+    "Serra Branca", "Serra Redonda", "Soledade", "Sossêgo", "Sumé",
+    "Taperoá", "Tenório", "Umbuzeiro", "Zabelê"
+  ],
+  "Macro 3": [
+    "Água Branca", "Aguiar", "Aparecida", "Belém do Brejo do Cruz",
+    "Bernardino Batista", "Boa Ventura", "Bom Jesus", "Bom Sucesso",
+    "Bonito de Santa Fé", "Brejo do Cruz", "Brejo dos Santos",
+    "Cachoeira dos Índios", "Cacimba de Areia", "Cacimbas", "Cajazeiras",
+    "Cajazeirinhas", "Carrapateira", "Catingueira", "Catolé do Rocha",
+    "Conceição", "Condado", "Coremas", "Curral Velho", "Diamante", "Emas",
+    "Ibiara", "Igaracy", "Imaculada", "Itaporanga", "Jericó", "Joca Claudino",
+    "Juru", "Lagoa", "Lastro", "Mãe d'Água", "Malta", "Manaíra", "Marizópolis",
+    "Mato Grosso", "Maturéia", "Monte Horebe", "Nazarezinho", "Nova Olinda",
+    "Olho d'Água", "Passagem", "Patos", "Paulista", "Pedra Branca", "Piancó",
+    "Poço Dantas", "Poço de José Moura", "Pombal", "Princesa Isabel",
+    "Quixabá", "Riacho dos Cavalos", "Salgadinho", "Santa Cruz", "Santa Helena",
+    "Santa Inês", "Santa Luzia", "Santa Teresinha", "Santana de Mangueira",
+    "Santana dos Garrotes", "São Bento", "São Domingos", "São Francisco",
+    "São João do Rio do Peixe", "São José da Lagoa Tapada", "São José de Caiana",
+    "São José de Espinharas", "São José de Piranhas", "São José de Princesa",
+    "São José do Bonfim", "São José do Brejo do Cruz", "São José do Sabugi",
+    "São Mamede", "Serra Grande", "Sousa", "Tavares", "Teixeira", "Triunfo",
+    "Uiraúna", "Várzea", "Vieirópolis", "Vista Serrana"
+  ]
+};
+
+export const TODAS_CIDADES = Object.values(CIDADES_POR_MACRO).flat().sort();
